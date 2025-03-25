3e23fa37bc33a1d1bfe9ebfdbaef63ec846224b6
Jiwonii97 <wldnjs3633@naver.com>
2025-03-26 00:04:22 +0900

    common) 0325 WIP

--- comon/0325/1012.py
+++ comon/0325/1012.py
@@ -1,41 +1,43 @@
 # www.acmicpc.net/problem/1012
 # 유기농 배추
-#
-#
+# 문제 예시와 실제 데이터가 조금 달라 헤맨 문제, BFS를 통해 풀이 가능
+# O(m × n)
 
 import sys
 t = int(sys.stdin.readline().strip())
 
-movement = [(0, -1), (1, 0), (0, 1), (-1, 0)]
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+
+
+def bfs(x, y):
+    dq = [(x, y)]
+    mat[x][y] = 0
+
+    while dq:
+        px, py = dq.pop(0)
+
+        for idx in range(4):
+            nx, ny = px+dx[idx], py+dy[idx]
+
+            if (0 <= nx < m and 0 <= ny < n) and mat[nx][ny] == 1:
+                dq.append((nx, ny))
+                mat[nx][ny] = 0
+
+
 for _ in range(t):
     m, n, k = map(int, sys.stdin.readline().strip().split())
-    mat = [[0]*m for _ in range(n)]
-    
+    mat = [[0]*n for _ in range(m)]
+
     for _ in range(k):
         x, y = map(int, sys.stdin.readline().strip().split())
-        mat[y][x] = 1
-    
-    visited = [[False]*m for _ in range(n)]
-    count = 0
-    for i in range(n):
-        for j in range(m):
-            print(f"> {i}, {j} / {mat[i][j]}")
-            
-            if mat[i][j] == 1:
-                mat[i][j] == 0
-                dq = [(i, j)]
-                while dq:
-                    print(">>> ",dq)
-                    px, py = dq.pop(0)
-                    mat[px][py] == 0
+        mat[x][y] = 1
 
-                    for dx, dy in movement:
-                        # 행렬의 조건을 만족
-                        if 0<=(px+dx)<n and 0<=(py+dy)<m:
-                            if mat[px+dx][py+dy] == 1:
-                                dq.append((px+dx,py+dy))
-                            
+    count = 0
+    for i in range(m):
+        for j in range(n):
+            if mat[i][j] == 1:
+                bfs(i, j)
                 count += 1
-            
-            visited[i][j] = True
+
     print(count)
