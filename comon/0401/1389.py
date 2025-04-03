17325f6807069f59455a88416dc7030f645687b7
Jiwonii97 <wldnjs3633@naver.com>
2025-04-03 12:43:17 +0900

    common) 0401 문제풀이2

--- comon/0401/1389.py
+++ comon/0401/1389.py
@@ -8,6 +8,22 @@ import sys
 input = sys.stdin.readline
 n, m = map(int, input().strip().split())
 
-INF = int(1e9)
+friend_map = [[n]*(n) for _ in range(n)]
 
-mat = [[INF]*(n+1) for _ in range(n+1)]
+for _ in range(m):
+    x, y = map(int, input().strip().split())
+    friend_map[x-1][y-1] = 1
+    friend_map[y-1][x-1] = 1
+
+for k in range(n):
+    for i in range(n):
+        for j in range(n):
+            if i==j:
+                friend_map[i][j] = 0
+            else:
+                friend_map[i][j] = min(friend_map[i][j], friend_map[i][k]+friend_map[k][j])
+
+friends = []
+for row in friend_map:
+    friends.append(sum(row))
+print(friends.index(min(friends)) + 1)
